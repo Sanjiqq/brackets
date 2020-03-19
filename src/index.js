b18742abe4b60b7374788e4a24868147a9197b10
Sanjiqq <60541344+Sanjiqq@users.noreply.github.com>
2020-03-19 22:47:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // function check(str, bracketsConfig)
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (!str.includes(bracketsConfig[i].join("")));
+        else {
+            str = str.replace(bracketsConfig[i].join(""), "");
+            i = 0;
+        }
+    }
+    if (str.length == 0) return true;
+    else return false;
+};
